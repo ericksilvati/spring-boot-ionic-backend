8b772c5682990159d9740b50bc56f09eef595e94
Erick Silva <ericksilvaconsultoriati@gmail.com>
2020-02-28 18:37:55 -0300

    Paginação com parametros opicionais na requisição

--- src/main/java/com/erick/cursomc/CursomcApplication.java
+++ src/main/java/com/erick/cursomc/CursomcApplication.java
@@ -64,7 +64,16 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-
+		Categoria cat3 = new Categoria(null, "Cama");
+		Categoria cat4 = new Categoria(null, "Mesa");
+		Categoria cat5 = new Categoria(null, "Banho");
+		Categoria cat6 = new Categoria(null, "Eletrônicos");
+		Categoria cat7 = new Categoria(null, "Jardinagem");
+		Categoria cat8 = new Categoria(null, "Decoração");
+		Categoria cat9 = new Categoria(null, "Pesca");
+		Categoria cat10= new Categoria(null, "Condomínio");
+		Categoria cat11= new Categoria(null, "Perfumaria");
+		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
@@ -76,7 +85,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		categoriaRepository.save(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
