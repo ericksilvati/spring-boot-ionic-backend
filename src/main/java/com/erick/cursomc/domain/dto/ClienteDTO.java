9f56e4dd871b37e936a71f134927bac57d3e07c4
Erick Silva <ericksilvaconsultoriati@gmail.com>
2020-02-29 11:02:11 -0300

    PUT, DELETE, GET para Cliente

---
+++ src/main/java/com/erick/cursomc/domain/dto/ClienteDTO.java
@@ -0,0 +1,51 @@
+package com.erick.cursomc.domain.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.erick.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	public ClienteDTO() {}
+	
+	public ClienteDTO (Cliente obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.setEmail(obj.getEmail());
+	}
+		
+	private Integer id;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Length(min=5, max=120, message="O tamenho deve ser entre 5 e 120 caracteres")
+	private String nome;
+	@NotEmpty(message="Preenchimento Obrigatório")
+	@Email (message="Email inválido")
+	private String email;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/erick/cursomc/services/ClienteService.java
+++ src/main/java/com/erick/cursomc/services/ClienteService.java
@@ -1,9 +1,18 @@
 package com.erick.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.erick.cursomc.domain.Cliente;
+import com.erick.cursomc.domain.Cliente;
+import com.erick.cursomc.domain.dto.ClienteDTO;
+import com.erick.cursomc.exceptions.DataIntegrityException;
 import com.erick.cursomc.exceptions.ObjectNotFoundException;
 import com.erick.cursomc.repositories.ClienteRepository;
 
@@ -12,16 +21,51 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
-	
-	
-	public Cliente buscar(Integer id) {
-		
+
+	public Cliente find(Integer id) {
+
 		Cliente obj = repo.findOne(id);
-		if (obj==null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: "+ id
-					+ ", Tipo: " + Cliente.class.getName());
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
 		}
-		
+
 		return obj;
+	}
+
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+			
+		
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
