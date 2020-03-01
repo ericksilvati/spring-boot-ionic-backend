d727e42ce6a76f73852fa381a751cb441771b394
Erick Silva <ericksilvaconsultoriati@gmail.com>
2020-03-01 15:59:54 -0300

    Validação customizada: email não repetindo na atualização do cliente

--- src/main/java/com/erick/cursomc/domain/dto/ClienteDTO.java
+++ src/main/java/com/erick/cursomc/domain/dto/ClienteDTO.java
@@ -7,7 +7,8 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.erick.cursomc.domain.Cliente;
-
+import com.erick.cursomc.services.validation.ClienteUpdate;
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
