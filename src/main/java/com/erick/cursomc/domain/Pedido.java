43dfc361367198142eb9888b34c6e0bf0a441883
Erick Silva <ericksilvaconsultoriati@gmail.com>
2020-05-30 21:54:00 -0300

    Implementando toString de pedido

--- src/main/java/com/erick/cursomc/domain/Pedido.java
+++ src/main/java/com/erick/cursomc/domain/Pedido.java
@@ -1,8 +1,13 @@
 package com.erick.cursomc.domain;
 
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -133,6 +138,30 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("Pedido número: ");
+		sb.append(getId());
+		sb.append(", Instante: ");
+		sb.append(sdf.format(getInstante()));
+		sb.append(", Cliente: ");
+		sb.append(getCliente().getNome());
+		sb.append(", Situação de pagamento: ");
+		sb.append(getPagamento().getEstado().getDescricao());
+		sb.append("\nDetalhes:\n");
+		for (ItemPedido ip : getItens())
+			sb.append(ip.toString());
+		sb.append("Valor total: ");
+		sb.append(nf.format(getValorTotal()));
+		
+		return sb.toString();
+	}
+	
 	
 	
 }
