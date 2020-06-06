30c6022c4b882da02dc29e7754e3754f9a5b933c
Erick Silva <ericksilvaconsultoriati@gmail.com>
2020-06-06 18:06:54 -0300

    Email HTML

--- src/main/java/com/erick/cursomc/services/EmailService.java
+++ src/main/java/com/erick/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.erick.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.erick.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
